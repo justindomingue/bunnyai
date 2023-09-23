1536c2d23b28b0813410ffed9583bb0691def04f
Justin Domingue <judo@uniswap.org>
2023-09-23 14:10:05 -0400

    improve slide

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen min-w-screen flex-col items-center justify-between p-8">
+    <main className="flex min-h-screen min-w-screen flex-col items-center justify-between">
       <Tabs defaultValue="feed" className="flex flex-col flex-1 gap-8">
         <TabsContent value="feed" className="flex-1">
           <Topic label="This is the topic" onTurn={function (): void {
@@ -18,7 +18,7 @@ export default function Home() {
         <TabsContent value="profile">
           <Profile />
         </TabsContent>
-        <TabsList>
+        <TabsList className="absolute bottom-8">
           <TabsTrigger value="feed">for you</TabsTrigger>
           <TabsTrigger value="profile">profile
           </TabsTrigger>
--- components/Topic.tsx
+++ components/Topic.tsx
@@ -3,6 +3,7 @@
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick-theme.css'
 import 'slick-carousel/slick/slick.css'
+import { darken } from 'polished'
 
 import { Noun } from "@/components/Noun"
 import { Button } from "@/components/ui/button"
@@ -10,7 +11,7 @@ import { createContext, useCallback, useContext, useState } from "react"
 
 const carouselSettings = {
     dots: false,
-    infinite: true,
+    infinite: false,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -24,7 +25,7 @@ const TopicContext = createContext<{
     label: string
     activeLevel: number
     contentList: Array<string>
-    onDeeper: () => void
+    onDeeper: (newLevel: number) => void
     onTurn: () => void
 }>({
     label: 'This is the topic',
@@ -38,54 +39,54 @@ export function Topic({ label, onTurn }: { label: string, onTurn: () => void }) 
     const [contentList, setContentList] = useState([])
     const [activeLevel, setActiveLevel] = useState(0)
 
-    const onDeeper = useCallback(() => setActiveLevel(activeLevel => activeLevel + 1), [])
+    const onDeeper = useCallback((newLevel: number) => setActiveLevel(newLevel), [])
 
     return <TopicContext.Provider value={{ label, activeLevel, contentList, onDeeper, onTurn }}>
-        <div className="flex-1 justify-between flex flex-col gap-6 absolute h-screen w-screen inset-0 p-8">
-            <div className='flex flex-col gap-4'>
-                {/* header */}
-                <div className="flex flex-row justify-between">
-                    <Noun />
-                    <Button>420 $honk</Button>
-                </div>
+        <Slider {...carouselSettings} className='h-full' afterChange={(level) => { setActiveLevel(level) }}>
+            <Section level={1} />
+            <Section level={2} />
+            <Section level={3} />
+        </Slider>
 
-                {/* topic */}
-                <div className="flex flex-col gap-1">
-                    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">{label}</h1>
-                    <p className="text-md text-muted-foreground">Level {activeLevel}</p>
-                </div>
+        <div className="justify-between flex flex-col gap-6 absolute inset-0 p-8 h-fit">
+            {/* header */}
+            <div className="flex flex-row justify-between">
+                <Noun />
+                <Button>420 $honk</Button>
             </div>
 
-            {/* actions */}
-            <div className="flex flex-row justify-between">
-                <Button>deeper</Button>
-                <Button>turn</Button>
+            {/* topic */}
+            <div className="flex flex-col gap-1">
+                <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">{label}</h1>
+                <p className="text-md text-muted-foreground">Level {activeLevel}</p>
             </div>
         </div>
-
-        {/* <div className='absolute inset-0'>
-            <Slider {...carouselSettings} className='h-full'>
-                <Section offset={-1} />
-                <Section offset={0} />
-                <Section offset={1} />
-            </Slider>
-        </div> */}
 
     </TopicContext.Provider >
 }
 
-function Section({ offset }: { offset: number }) {
+function Section({ level }: { level: number }) {
     const { contentList, activeLevel } = useContext(TopicContext)
-    const content = contentList[activeLevel + offset]
+    const backgroundColor = darken(level * 0.05, '#FFCD64')
 
-    return <div className="flex flex-col gap-4 font-bold px-8 mt-44 h-screen">
-        <p className="text-lg">This is the body text about the rabbit hole.</p>
-        <p className="text-lg">This is the body text about the rabbit hole.</p>
-        <p className="text-lg">This is the body text about the rabbit hole.</p>
-        <p className="text-lg">This is the body text about the rabbit hole.</p>
-        <p className="text-lg">This is the body text about the rabbit hole.</p>
-        <p className="text-lg">This is the body text about the rabbit hole.</p>
-        <p className="text-lg">This is the body text about the rabbit hole.</p>
-        <p className="text-lg">This is the body text about the rabbit hole.</p>
+    return <div className='flex flex-col gap-4 px-8 h-screen w-screen' style={{ backgroundColor }}>
+        <div className="flex flex-col gap-4 font-bold mt-44">
+            <p className="text-lg">This is the body text about the rabbit hole.</p>
+            <p className="text-lg">This is the body text about the rabbit hole.</p>
+            <p className="text-lg">This is the body text about the rabbit hole.</p>
+            <p className="text-lg">This is the body text about the rabbit hole.</p>
+            <p className="text-lg">This is the body text about the rabbit hole.</p>
+            <p className="text-lg">This is the body text about the rabbit hole.</p>
+            <p className="text-lg">This is the body text about the rabbit hole.</p>
+            <p className="text-lg">This is the body text about the rabbit hole.</p>
+
+        </div>
+
+        {/* actions */}
+        <div className="flex flex-row justify-between">
+            <Button>deeper</Button>
+            <Button>turn</Button>
+        </div>
     </div>
+
 }
