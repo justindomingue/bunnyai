9ac0126693b6cec1a100739edf678b80d2b5a7fc
spenciefy <spencersyen@gmail.com>
2023-09-24 01:34:18 -0400

    Add noun logo svg

--- components/ui/NounImage.tsx
+++ components/ui/NounImage.tsx
@@ -1,10 +1,8 @@
 import { ImageData, getNounData } from '@nouns/assets'
 import { buildSVG } from '@nouns/sdk'
-import Image from 'next/image'
 import seedrandom from 'seedrandom'
-import { Avatar } from './avatar'
 
-const NounImage = ({ prompt = 'happiness', size = 100 }) => {
+const NounImage = ({ isLogo = false, prompt = 'happiness', size = 100 }) => {
   const {
     palette,
     // images
@@ -21,13 +19,21 @@ const NounImage = ({ prompt = 'happiness', size = 100 }) => {
 
   const rng = seedrandom(prompt)
 
-  const seed = {
-    background: Math.floor(rng() * 2),
-    body: Math.floor(rng() * 30),
-    accessory: Math.floor(rng() * 142),
-    head: Math.floor(rng() * 250),
-    glasses: Math.floor(rng() * 23),
-  }
+  const seed = isLogo
+    ? {
+        background: 1,
+        body: 28, // slimegreen
+        accessory: 24, // carrot
+        head: 169, // rabbit head
+        glasses: 20, // yellow
+      }
+    : {
+        background: Math.floor(rng() * 2),
+        body: Math.floor(rng() * 30),
+        accessory: Math.floor(rng() * 142),
+        head: 169, // rabbit head
+        glasses: Math.floor(rng() * 23),
+      }
 
   const { parts, background } = getNounData(seed)
 
@@ -35,14 +41,14 @@ const NounImage = ({ prompt = 'happiness', size = 100 }) => {
   const svgBase64 = btoa(svgBinary)
 
   return (
-    <Avatar>
-      <Image
-        src={`data:image/svg+xml;base64,${svgBase64}`}
-        alt={`${prompt} noun`}
-        width={size}
-        height={size}
-      />
-    </Avatar>
+    // eslint-disable-next-line @next/next/no-img-element
+    <img
+      src={`data:image/svg+xml;base64,${svgBase64}`}
+      alt={`${prompt} noun`}
+      width={size}
+      height={size}
+      style={{ borderRadius: '50%' }}
+    />
   )
 }
 
