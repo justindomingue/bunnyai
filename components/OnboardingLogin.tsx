52f7dbc7a74216decd3b28f184849cb9192aee5c
spenciefy <spencersyen@gmail.com>
2023-09-24 01:48:21 -0400

    Clean up OnboardingLogin

--- components/OnboardingLogin.tsx
+++ components/OnboardingLogin.tsx
@@ -4,90 +4,17 @@ import 'slick-carousel/slick/slick-theme.css'
 import 'slick-carousel/slick/slick.css'
 
 import { Button } from '@/components/ui/button'
-import {
-  BiconomySmartAccountV2,
-  DEFAULT_ENTRYPOINT_ADDRESS,
-} from '@biconomy/account'
-import { Bundler, IBundler } from '@biconomy/bundler'
-import { ChainId } from '@biconomy/core-types'
-import {
-  DEFAULT_ECDSA_OWNERSHIP_MODULE,
-  ECDSAOwnershipValidationModule,
-} from '@biconomy/modules'
-import { BiconomyPaymaster, IPaymaster } from '@biconomy/paymaster'
-import { ConnectedWallet, usePrivy, useWallets } from '@privy-io/react-auth'
-import { ethers } from 'ethers'
-import { useEffect, useState } from 'react'
+import { BiconomySmartAccountV2 } from '@biconomy/account'
+import { usePrivy } from '@privy-io/react-auth'
 import { NounImage } from './ui/NounImage'
 
 export let smartAccount: BiconomySmartAccountV2 | null = null
 
 export function OnboardingLogin({}: {}) {
-  const { ready, login, logout, authenticated, user } = usePrivy()
-  const { wallets } = useWallets()
-  const [wallet, setWallet] = useState<ConnectedWallet | undefined>(undefined)
-  const [address, setAddress] = useState<string>('')
-  const [loading, setLoading] = useState<boolean>(false)
-  const [provider, setProvider] = useState<ethers.providers.Provider | null>(
-    null
-  )
-
-  const bundler: IBundler = new Bundler({
-    bundlerUrl: process.env.NEXT_PUBLIC_BUNDLER_URL!,
-    chainId: ChainId.BASE_MAINNET,
-    entryPointAddress: DEFAULT_ENTRYPOINT_ADDRESS,
-  })
-
-  const paymaster: IPaymaster = new BiconomyPaymaster({
-    paymasterUrl: process.env.NEXT_PUBLIC_PAYMASTER_URL!,
-  })
-
-  // when authenticated, push new screen
-  useEffect(() => {
-    if (authenticated) {
-      // push new screen
-    }
-  }, [authenticated])
+  const { login } = usePrivy()
 
   const onPressCreateBunny = () => {
     login()
-  }
-
-  const connect = async () => {
-    try {
-      const embeddedWallet = wallets.find(
-        (wallet) => wallet.walletClientType === 'privy'
-      )
-      if (!embeddedWallet) {
-        throw new Error('Privy wallet not found')
-      }
-      setWallet(embeddedWallet)
-
-      const customProvider = new ethers.providers.Web3Provider(
-        await embeddedWallet.getEthereumProvider()
-      )
-      setProvider(customProvider)
-
-      // set privy wallet as signer
-      const biconomyModule = await ECDSAOwnershipValidationModule.create({
-        signer: customProvider.getSigner(),
-        moduleAddress: DEFAULT_ECDSA_OWNERSHIP_MODULE,
-      })
-
-      smartAccount = await BiconomySmartAccountV2.create({
-        chainId: ChainId.BASE_MAINNET,
-        bundler: bundler,
-        paymaster: paymaster,
-        entryPointAddress: DEFAULT_ENTRYPOINT_ADDRESS,
-        defaultValidationModule: biconomyModule,
-        activeValidationModule: biconomyModule,
-      })
-      setAddress(await smartAccount.getAccountAddress())
-      console.log('smartAccount', smartAccount)
-      setLoading(false)
-    } catch (error) {
-      console.error(error)
-    }
   }
 
   return (
